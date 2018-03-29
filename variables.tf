14d0585a98632cfb61586507b06a77bb6d236098
Alessandro Furkim <alessandrofurkim@Alessandros-MacBook-Pro-2.local>
2018-03-29 19:49:04 +0200

    clean up

--- variables.tf
+++ variables.tf
@@ -1,28 +1,10 @@
-variable "stack" {
-  type = "string"
-  default = "crab_io"
-}
+
 variable "region" {
   type = "string"
   default = "eu-central-1"
 }
-variable "git_project" {
-  type = "string"
-  default ="setup"
 
-}
-variable "port" {
-  type = "string"
-  default = "3000",
-}
-variable "version" {
-  type = "string"
-  default = "0.1"
-}
-variable "environment" {
-  type = "string"
-  default = "dev"
-}variable "acc_key" {
+variable "acc_key" {
   type = "string"
   default = "dev"
 }variable "sec_key" {
