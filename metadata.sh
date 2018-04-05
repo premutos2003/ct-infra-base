9ed6b667dba0975fa4cd7b1e91ce55dc2f4fe659
Alessandro Furkim <alessandrofurkim@Alessandros-MacBook-Pro-2.local>
2018-04-05 23:35:13 +0200

    local

--- metadata.sh
+++ metadata.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-terraform output -json  | curl -H 'Content-Type: application/json' -d @- http://localhost:3000/infra
+terraform output -json  | curl -H 'Content-Type: application/json' -d @- docker.for.mac.localhost:3000/infra
