59467537c8e8bcb0ca28ff213ba91e1deba511d1
Alessandro Furkim <alessandrofurkim@Alessandros-MacBook-Pro-2.local>
2018-04-05 21:57:07 +0200

    delete retention set to 1

--- kms.tf
+++ kms.tf
@@ -1,4 +1,5 @@
 resource "aws_kms_key" "kms_key" {
+  deletion_window_in_days = 1
   description = "Encryption key for storing secrets"
 }
  output "kms_key_arn" {
--- variables.tf
+++ variables.tf
@@ -3,11 +3,11 @@ variable "region" {
   type = "string"
   default = "eu-central-1"
 }
-
 variable "aws_access_key" {
   type = "string"
   default = "dev"
-}variable "aws_secret_key" {
+}
+variable "aws_secret_key" {
   type = "string"
   default = "dev"
 }
