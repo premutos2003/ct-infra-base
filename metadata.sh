71d0ba20ef25d0a2e818eee22fc7d1bd75ae43ca
Alessandro Furkim <alessandrofurkim@Alessandros-MacBook-Pro-2.local>
2018-04-16 23:48:21 +0200

    fix

--- metadata.sh
+++ metadata.sh
@@ -6,3 +6,6 @@ terraform output -json  | curl -H 'Content-Type: application/json' -d @- localho
 var=$(terraform output -json | jq -r '.s3_bucket.value')
 aws s3 cp ./terraform.tfstate s3://$var/state/terraform.tfstate  --region ${REGION}
 aws s3 cp  ./terraform.tfstate.backup s3://$var/state/terraform.tfstate.backup --region ${REGION}
+
+
+
